e3f2791b179e34eed066c839e5802d77ab6544f8
Tanvir Rahman <tanvirrahman@pop-os.localdomain>
2020-08-24 20:22:38 +0600

    added

--- course2/first/mysite/polls/views.py
+++ course2/first/mysite/polls/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http      import HttpResponse,Http404
+from django.shortcuts import render
+from .models          import Question
 
 # Create your views here.
+def owner(request):
+    return HttpResponse("Hello, world. ba4002d8 is the polls index.");
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output               = '.'.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    context = {"latest_question_list":latest_question_list}
+    return render(request,'polls/index.html',context)
+
+def details(request,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exists")
+
+    return render(request,'polls/detail.html',{'question':question})
